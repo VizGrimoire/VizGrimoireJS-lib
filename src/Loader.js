40dfad8d7cc8a46e7a750f4a6256a32692ff4744
Luis Cañas-Díaz <lcanas@bitergia.com>
2014-07-11 14:25:20 +0200

    Update header with copyright + authorship (2nd attempt)

--- src/Loader.js
+++ src/Loader.js
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2012-2013 Bitergia
+ * Copyright (C) 2012-2014 Bitergia
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@
  *
  * Authors:
  *   Alvaro del Castillo San Felix <acs@bitergia.com>
+ *   Luis Cañas-Díaz <lcanas@bitergia.com>
  */
 
 if (Loader === undefined) var Loader = {};
