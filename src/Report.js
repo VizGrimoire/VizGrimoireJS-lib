60fd58f519f8b8e8e16f05f9b3423ee30883d6fd
Alvaro del Castillo <acs@bitergia.com>
2014-09-26 12:39:36 +0200

    Don't use Convert.convertModifiedBasicMetrics until it is fixed.

--- src/Report.js
+++ src/Report.js
@@ -591,7 +591,7 @@ Loader.data_ready(function(){
     // but .. are the tops by repos already assigned? -> we need a check
     study = "repos";
     Convert.convertFilterTop(study);
-    Convert.convertModifiedBasicMetrics(study);
+    // Convert.convertModifiedBasicMetrics(study);
 });
 
 Loader.data_ready(function() {
